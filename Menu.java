bdaf351d59e6a7d0e954b23ae70d01d1dc4945eb
jazzyzoney <jaqtash22@gmail.com>
2024-04-09 11:58:04 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Menu{
 
    static ArrayList<Pizza> menu = new ArrayList<>();
-
+   
 //name, price, number
    public Menu(){
-      Menu.add(new Pizza("Vesuvio", 57, 1));
-      Menu.add(new Pizza("Amerikaner", 53, 2));
-      Menu.add(new Pizza("Cacciatore", 57, 3));
+      menu.add(new Pizza("Vesuvio", 57, 1));
+      menu.add(new Pizza("Amerikaner", 53, 2));
+      menu.add(new Pizza("Cacciatore", 57, 3));
       
       
    }
