e9e6873688fa2daf4e4aabfb3c78afb994eccc57
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-04-09 11:45:34 +0200

    Orders edit 1

--- Orders.java
+++ Orders.java
@@ -1,3 +1,15 @@
-public class Orders{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Orders{
+   static ArrayList<Pizza> orderList = new ArrayList <Pizza>();
+   static Scanner scanner = new Scanner(System.in);
+   
+   public static void addOrder(int addPizzas){
+      for(int i = 0; i <= addPizzas; i++){
+         System.out.println("Pizza nr: ");
+         int pizzaNo = scanner.nextInt();
+         orderList.add(Menu.menu.get(pizzaNo));
+      }
+   }
 }
