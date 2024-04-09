05b46f240e946987ca8f799c9542c6274332ed71
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-04-09 11:23:47 +0200

    pizza class edit 1

--- Pizza.java
+++ Pizza.java
@@ -1,5 +1,11 @@
 public class Pizza{
-int price =0; 
-int number=0; 
-String name=""; 
+   int price =0; 
+   int number; 
+   String name=""; 
+   
+   public Pizza(String name, int price, int number){
+      this.name = name;
+      this.price = price;
+      this.number = number;
+   }
 }
