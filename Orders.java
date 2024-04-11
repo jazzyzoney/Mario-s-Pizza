5dbc83fb254ad9399550688fb19e296b3e57834b
jazzyzoney <jaqtash22@gmail.com>
2024-04-11 12:12:06 +0200

    date

--- Orders.java
+++ Orders.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
-
+import java.text.SimpleDateFormat;
 
 public class Orders{
    //datatype time her
+   SimpleDateFormat date = new SimpleDateFormat("Y/MM/dd HH:mm");
    String name;
    ArrayList<Pizza> pizzaList = new ArrayList<Pizza>();
    
    public Orders(String name){
-      //this.time = time;
+      this.date = date;
       this.name = name;
    }
 }
