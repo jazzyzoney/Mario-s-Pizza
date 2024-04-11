de58548e36e7025b0eeb6d95ab25a809a68a165f
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-04-11 10:25:04 +0200

    TextUI class oprettet

--- Main.java
+++ Main.java
@@ -2,50 +2,8 @@ import java.util.Scanner;
 import javax.swing.*;
 
 public class Main{
-   static Scanner scanner = new Scanner (System.in);
-   public static void textUI (){
-      String Letters = ""; 
-      System.out.print("Mamma mia velkommen til Mario's pizza order system");
-      Menu.addPizzamenu();
-      Menu.printMenu();
-      System.out.print("du har nu fire valgmuligheder");
-            System.out.print("add" + "" + "som tilf�jer en ny order" + "" + "done" + ""+ "done som f�rdigg�r orderen"+ "" + "view" + "hvor man kan se ordre"+ "" + "q" + "som lukker programet");
-      
-      Letters = scanner.next();
-      switch (Letters){
-      
-         case "add":
-         int ordernumber = scanner.nextInt();
-         Orders.addOrder(ordernumber);
-         break; 
-         
-         case "done":
-         int sletnummer = scanner.nextInt();
-         Orders.completeOrder(sletnummer);
-         break; 
-         
-         case "delete":
-          int deletenumber = scanner.nextInt();
-         Orders.deleteOrder(deletenumber);
-         break;
-         
-         case "view":         
-        Orders.printOrders();
-         break;
-
-         case "Q":
-         System.exit(0); 
-      }
-   }
-      
-      //Orders.addOrder();
-      //Orders.completeOrder();
-      //Orders.deleteOrder();
-      
-      //Orders.printOrders();
-
    public static void main (String [] args){
-      //textUI();
+      TextUI.textUI();
       
       SwingUtilities.invokeLater(()->{ 
          new UI();
---
+++ TextUI.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class TextUI{
+   static Scanner scanner = new Scanner (System.in);
+   public static void textUI (){
+      String Letters = ""; 
+      System.out.print("Mamma mia velkommen til Mario's pizza order system");
+      Menu.addPizzamenu();
+      Menu.printMenu();
+      System.out.print("du har nu fire valgmuligheder");
+            System.out.print("add" + "" + "som tilf�jer en ny order" + "" + "done" + ""+ "done som f�rdigg�r orderen"+ "" + "view" + "hvor man kan se ordre"+ "" + "q" + "som lukker programet");
+      
+      Letters = scanner.next();
+      switch (Letters){
+      
+         case "add":
+            int ordernumber = scanner.nextInt();
+            Orders.addOrder(ordernumber);
+            break; 
+         
+         case "done":
+            int sletnummer = scanner.nextInt();
+            Orders.completeOrder(sletnummer);
+            break; 
+         
+         case "delete":
+            int deletenumber = scanner.nextInt();
+            Orders.deleteOrder(deletenumber);
+            break;
+         
+         case "view":         
+            Orders.printOrders();
+            break;
+
+         case "Q":
+            System.exit(0); 
+      }
+   }
+}
