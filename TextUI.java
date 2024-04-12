5137417490e4ba02e00d886a56f35d9aaee405ef
jazzyzoney <jaqtash22@gmail.com>
2024-04-12 13:03:57 +0200

    mere text

--- TextUI.java
+++ TextUI.java
@@ -4,17 +4,17 @@ public class TextUI{
    static Scanner scanner = new Scanner (System.in);
    public static void textUI (){
       String Letters = ""; 
-      System.out.print("Mamma mia velkommen til Mario's pizza order system");
-      Menu.addPizzamenu();
+      System.out.print("Mamma mia velkommen til Mario's pizza order system!\n");
       Menu.addPizzamenu();
       do {
          System.out.print("\nDu har nu fire valgmuligheder:");
          System.out.println("");
-         System.out.println("Du kan nu skrive" + " add " + "for at addere en ordre,");
-         System.out.println("eller skrive" + " done " + "for at afslutte ordreren,");
-         System.out.println("delete " + "vil fjerne ordreren fra systemet,");
-         System.out.println("view " + "vil lade dig se alle ordrerne,");
-         System.out.println("q " + "afslutter programmet.");
+         System.out.println("Du kan nu skrive [add] for at addere en ordre,");
+         System.out.println("eller skrive [done] for at afslutte ordreren,");
+         System.out.println("[delete] vil fjerne ordreren fra systemet,");
+         System.out.println("[view] vil lade dig se alle ordrerne,");
+         System.out.println("[statistik] giver et overblik over statistikken,");
+         System.out.println("[q] afslutter programmet.");
       
          Letters = scanner.next();
          switch (Letters){
@@ -29,22 +29,27 @@ public class TextUI{
                System.out.println("Hvilken ordrer er done?");
                int sletnummer = scanner.nextInt();
                OrderList.completeOrder(sletnummer);
-               System.out.println("Ordreren"+ ""+ sletnummer+""+ "er nu blevet markeret som done og gemt til statestikken");
+               System.out.println("Ordreren" + "" + sletnummer + "" + "er nu blevet markeret som done og gemt til statistikken");
                break; 
          
             case "delete":
                System.out.println("Hvilken ordrer skal slettes?");
                int deletenumber = scanner.nextInt();
                OrderList.deleteOrder(deletenumber);
-               System.out.println("Ordreren"+ ""+ deletenumber+""+ "er nu blevet markeret som slettet og er fjernet fra dell computeren");
+               System.out.println("Ordreren " + "  " + deletenumber + "  " + " er nu blevet markeret som slettet og er fjernet fra dell computeren");
                break;
          
             case "view":         
                OrderList.printOrders();
                break;
+               
+            case "statistik":
+               Statistics.turnover();
+               Statistics.favoritePizza();
+            break;
          
             case "q":
-               System.out.println("Tak for idag");
+               System.out.println("Tak for idag :)");
                System.exit(0);        
          }
       } while(true);
