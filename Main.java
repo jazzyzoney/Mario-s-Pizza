4c87289e4b9a85d7518b2c864d60d6c1c65db851
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-04-15 13:24:15 +0200

    ordrer kan tilføjes fra swing GUI

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ import javax.swing.*;
 public class Main{
    
    public static void main (String [] args){
-      //Menu.addPizzamenu(); 
+      Menu.addPizzamenu(); 
       //Scanner scanner = new Scanner(System.in);
       //int menuChoice = scanner.nextInt();
       //Menu.pizzaMenuCard(menuChoice);
    
-     // OrderList.addOrder(2);
+      //OrderList.addOrder(2);
      // OrderList.printOrders();
       //TextUI.textUI();q
       
       
-     // SwingUtilities.invokeLater(()->{ 
-        // new UI();  
-      //});
-      TextUI.textUI ();
+     SwingUtilities.invokeLater(()->{ 
+         new UI();  
+      });
+      //TextUI.textUI ();
    }
    
 }
